77dfce9740d2466a0f54e77207c7887c04354147
Josh Freed <josh@jpfreed.com>
2017-08-30 16:48:46 -0400

    Initial Commit

---
+++ JFLib/JFLib.h
@@ -0,0 +1,19 @@
+//
+//  JFLib.h
+//  JFLib
+//
+//  Created by Josh Freed on 8/30/17.
+//  Copyright © 2017 Josh Freed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JFLib.
+FOUNDATION_EXPORT double JFLibVersionNumber;
+
+//! Project version string for JFLib.
+FOUNDATION_EXPORT const unsigned char JFLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JFLib/PublicHeader.h>
+
+
