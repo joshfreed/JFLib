ad534d984c995b087be342addafaf9584e81ce60
Josh Freed <josh@jpfreed.com>
2021-12-06 10:44:40 -0500

    Tweaked domain events to be able to add events that aren't published immediately

--- Sources/JFLib.DomainEvents/DomainEventPublisher.swift
+++ Sources/JFLib.DomainEvents/DomainEventPublisher.swift
@@ -4,17 +4,38 @@ public class DomainEventPublisher {
     public static let shared = DomainEventPublisher()
 
     private var subscribers: [DomainEventSubscriberProto] = []
+    private var pendingHandlers: [() -> Void] = []
 
+    /// Immediately publishes the given `DomainEvent` to subscribers.
     public func publish<T: DomainEvent>(_ domainEvent: T) {
         subscribers
             .filter { $0.eventType == T.self }
             .forEach { $0.handleEvent(domainEvent) }
     }
 
+    /// Adds a `DomainEvent` that will be published later. Call `publishPendingEvents` to immediately publish all added events.
+    ///
+    /// Use this method when you want domain events to only be published if a transaction succeeds, for example saving to a database.
+    public func add<T: DomainEvent>(_ domainEvent: T) {
+        pendingHandlers = subscribers
+            .filter { $0.eventType == T.self }
+            .map { subscriber in { subscriber.handleEvent(domainEvent) } }
+    }
+
+    /// Immediately publishes domain events added by the `add` method. Once published, the domain events are cleared.
+    public func publishPendingEvents() {
+        for handler in pendingHandlers {
+            handler()
+        }
+        pendingHandlers = []
+    }
+
+    /// Attach a subscriber that will be fired when a domain event matching the specified type is published.
     public func subscribe<T: DomainEvent>(_ subscriber: DomainEventSubscriber<T>) {
         subscribers.append(subscriber)
     }
 
+    /// Clears all subscribers. Subscribers will remain attached forever unless this func is called.
     public func reset() {
         subscribers = []
     }
--- Tests/JFLib.DomainEventsTests/DomainEventPublishingTests.swift
+++ Tests/JFLib.DomainEventsTests/DomainEventPublishingTests.swift
@@ -35,6 +35,26 @@ class DomainEventPublishingTests: XCTestCase {
         XCTAssertEqual(1, events[0].id)
         XCTAssertEqual(2, events[1].id)
     }
+
+    func test_delayed_publish() {
+        var events: [TestEvent1] = []
+        DomainEventPublisher.shared.subscribe(DomainEventSubscriber<TestEvent1> { domainEvent in
+            events.append(domainEvent)
+        })
+
+        DomainEventPublisher.shared.add(TestEvent1(id: 1))
+
+        XCTAssertEqual(0, events.count)
+
+        DomainEventPublisher.shared.publishPendingEvents()
+
+        XCTAssertEqual(1, events.count)
+        XCTAssertEqual(1, events[0].id)
+
+        DomainEventPublisher.shared.publishPendingEvents()
+
+        XCTAssertEqual(1, events.count)
+    }
 }
 
 struct TestEvent1: DomainEvent {
