aa0b25898b72191df09e791b2176897d96f59c97
Josh Freed <josh@jpfreed.com>
2021-12-06 11:21:23 -0500

    Made func public

--- Sources/JFLib.DomainEvents/DomainEvent.swift
+++ Sources/JFLib.DomainEvents/DomainEvent.swift
@@ -5,7 +5,7 @@ public protocol DomainEvent {}
 /// Helper class for domain entities to append events that will be published after the application transaction commits.
 public final class DomainEvents {
     /// Adds a domain event raised by the domain
-    static func add<T: DomainEvent>(_ domainEvent: T) {
+    public static func add<T: DomainEvent>(_ domainEvent: T) {
         DomainEventPublisher.shared.add(domainEvent)
     }
 }
